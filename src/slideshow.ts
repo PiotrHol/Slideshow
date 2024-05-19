93c8b9e26bb503449cd1f96beaaf4a444ca9bd64
Piotr Hołub <piotrekholub@wp.pl>
2024-05-19 15:50:12 +0200

    Added slideshow's initialization

--- src/slideshow.ts
+++ src/slideshow.ts
@@ -1,9 +1,31 @@
+const slideshowContainerClass = "slideshow-container";
+
 interface Slideshow {
   init: () => void;
 }
 
 class SlideshowSlider implements Slideshow {
-  constructor() {}
+  private slideshowSelector;
+  private isInit: boolean;
 
-  init() {}
+  constructor(slideshowSelector: HTMLElement) {
+    this.slideshowSelector = slideshowSelector;
+    this.isInit = false;
+  }
+
+  init() {
+    if (!this.isInit) {
+      this.isInit = true;
+    }
+  }
+}
+
+const nodeSlideshowsContainers: NodeListOf<HTMLElement> =
+  document.querySelectorAll(`.${slideshowContainerClass}`);
+
+const slideshowsContainers: SlideshowSlider[] = [];
+
+for (let i = 0; i < nodeSlideshowsContainers.length; i++) {
+  slideshowsContainers.push(new SlideshowSlider(nodeSlideshowsContainers[i]));
+  slideshowsContainers[slideshowsContainers.length - 1].init();
 }
