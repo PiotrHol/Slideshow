8ff9e6a9fb9b7f4d1fdc00212aa871f847e8cb8a
Piotr Hołub <piotrekholub@wp.pl>
2024-05-20 20:27:22 +0200

    Added body of SlideshowSlider init function

--- src/slideshow.ts
+++ src/slideshow.ts
@@ -1,21 +1,58 @@
 const slideshowContainerClass = "slideshow-container";
 
+type slideshowElement = {
+  element: HTMLElement;
+  height: number;
+  width: number;
+};
+
 interface Slideshow {
   init: () => void;
 }
 
 class SlideshowSlider implements Slideshow {
-  private slideshowSelector;
+  private slideshowNode;
   private isInit: boolean;
+  private slideshowElements: slideshowElement[];
+  private maxHeight: number;
 
-  constructor(slideshowSelector: HTMLElement) {
-    this.slideshowSelector = slideshowSelector;
+  constructor(slideshowNode: HTMLElement) {
+    this.slideshowNode = slideshowNode;
     this.isInit = false;
+    this.slideshowElements = [];
+    this.maxHeight = 0;
   }
 
   init() {
     if (!this.isInit) {
       this.isInit = true;
+      document.addEventListener("DOMContentLoaded", () => {
+        if (this.slideshowNode.dataset.maxWidth) {
+          this.slideshowNode.style.maxWidth = `${Number(
+            this.slideshowNode.dataset.maxWidth
+          )}px`;
+        }
+
+        const slideshowChildren = Array.from(
+          this.slideshowNode.children
+        ) as HTMLElement[];
+
+        for (const slideshowChild of slideshowChildren) {
+          const slideshowChildData = {
+            element: slideshowChild,
+            height: slideshowChild.offsetHeight,
+            width: slideshowChild.offsetWidth,
+          };
+          this.slideshowElements.push(slideshowChildData);
+          if (slideshowChildData.height > this.maxHeight) {
+            this.maxHeight = slideshowChildData.height;
+          }
+        }
+
+        if (this.maxHeight && this.maxHeight > 0) {
+          this.slideshowNode.style.height = `${this.maxHeight}px`;
+        }
+      });
     }
   }
 }
